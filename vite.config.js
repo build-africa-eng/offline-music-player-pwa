baac5384591e6bcf95678c54d359a9a7314164f7
build-africa-eng <afrcanfuture@gmail.com>
2025-05-31 00:36:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss'
+limport { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+  },
 });
