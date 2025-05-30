9117c540e4ccd11470101a462f754c95a2087e82
build-africa-eng <afrcanfuture@gmail.com>
2025-05-31 00:44:52 +0300

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 function Player({ file }) {
   return (
-    <div className="p-4 bg-gray-200">
-      {file && (
-        <audio
-          src={URL.createObjectURL(file)}
-          controls
-          className="w-full"
-        ></audio>
+    <div className="p-4 bg-background text-text">
+      <h2 className="text-2xl font-bold mb-4">Player</h2>
+      {file ? (
+        <audio src={URL.createObjectURL(file)} controls className="w-full" />
+      ) : (
+        <p className="text-text">No song selected</p>
       )}
     </div>
   );
 }
+
 export default Player;
