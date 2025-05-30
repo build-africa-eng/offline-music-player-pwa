56c0a0960d31cb867964f446924faea829e123d8
build-africa-eng <afrcanfuture@gmail.com>
2025-05-30 22:55:55 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">Offline Music Player</h1>
+    </div>
+  );
+}
+export default App;
