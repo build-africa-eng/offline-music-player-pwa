5eb198cf4dba2eeae66525d1d52bf1c78f99f940
build-africa-eng <afrcanfuture@gmail.com>
2025-05-30 23:06:50 +0300

    Create Playlist.jsx

---
+++ src/components/Playlist.jsx
@@ -0,0 +1,9 @@
+function Playlist() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Playlists</h2>
+      <p className="text-gray-600">Add songs to create a playlist</p>
+    </div>
+  );
+}
+export default Playlist;
