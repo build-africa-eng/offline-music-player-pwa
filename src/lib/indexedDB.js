8b7291ea5224705b481164d2e2f6f899e65d3943
build-africa-eng <afrcanfuture@gmail.com>
2025-06-02 00:04:42 +0300

    Update indexedDB.js

--- src/lib/indexedDB.js
+++ src/lib/indexedDB.js
@@ -1,21 +1,26 @@
 import { openDB } from 'idb';
 
-const DB_NAME = 'SongsDB';
+const DB_NAME = 'music-db';
+const DB_VERSION = 2;
 const SONGS_STORE = 'songs';
 const PLAYLISTS_STORE = 'playlists';
 
 async function getDb() {
-  return await openDB(DB_NAME, 1, {
-    upgrade(db) {
-      if (!db.objectStoreNames.contains(SONGS_STORE)) {
+  return openDB(DB_NAME, DB_VERSION, {
+    upgrade(db, oldVersion) {
+      if (oldVersion < 1) {
         db.createObjectStore(SONGS_STORE, { keyPath: 'id' });
-      }
-      if (!db.objectStoreNames.contains(PLAYLISTS_STORE)) {
         db.createObjectStore(PLAYLISTS_STORE, { keyPath: 'id' });
+      } else if (oldVersion < 2) {
+        if (!db.objectStoreNames.contains(PLAYLISTS_STORE)) {
+          db.createObjectStore(PLAYLISTS_STORE, { keyPath: 'id' });
+        }
       }
-    },
+    }
   });
 }
+
+// SONGS
 
 export async function addSong(song) {
   const db = await getDb();
@@ -24,8 +29,15 @@ export async function addSong(song) {
 
 export async function getSongs() {
   const db = await getDb();
-  return await db.getAll(SONGS_STORE);
+  return db.getAll(SONGS_STORE);
 }
+
+export async function getSongById(id) {
+  const db = await getDb();
+  return db.get(SONGS_STORE, id);
+}
+
+// PLAYLISTS
 
 export async function createPlaylist(name, songIds = []) {
   const db = await getDb();
@@ -34,9 +46,16 @@ export async function createPlaylist(name, songIds = []) {
   return id;
 }
 
+export async function addPlaylist(playlist) {
+  const db = await getDb();
+  const id = playlist.id || crypto.randomUUID();
+  await db.put(PLAYLISTS_STORE, { ...playlist, id });
+  return id;
+}
+
 export async function getPlaylists() {
   const db = await getDb();
-  return await db.getAll(PLAYLISTS_STORE);
+  return db.getAll(PLAYLISTS_STORE);
 }
 
 export async function updatePlaylist(playlist) {
