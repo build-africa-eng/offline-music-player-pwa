17c06790d0bae74ca559b9703be74ea79b054479
build-africa-eng <afrcanfuture@gmail.com>
2025-06-04 16:49:44 +0300

    Update PlayerFooter.jsx

--- src/components/PlayerFooter.jsx
+++ src/components/PlayerFooter.jsx
@@ -3,8 +3,15 @@ import Waveform from './Waveform';
 
 function PlayerFooter({ currentId, songId, metadata, progress, duration, waveform }) {
   return (
-    <footer className="hidden lg:flex flex-col gap-4 w-full p-     <LyricsDisplay key={currentId} songId={songId} title={metadata.title} artist={metadata.artist} progress={progress} />}
-      <Waveform waveformData={waveform} progress={progress} duration={duration} />}
+    <footer className="hidden lg:flex flex-col gap-4 w-full p-4 bg-gray-200 dark:bg-gray-800 rounded-lg">
+      <LyricsDisplay
+        key={currentId}
+        songId={songId}
+        title={metadata.title}
+        artist={metadata.artist}
+        progress={progress}
+      />
+      <Waveform waveformData={waveform} progress={progress} duration={duration} />
     </footer>
   );
 }
