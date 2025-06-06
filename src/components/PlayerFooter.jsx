834bc1ad91e963540101f91249004323e90e6879
build-africa-eng <afrcanfuture@gmail.com>
2025-06-06 23:41:01 +0300

    Update PlayerFooter.jsx

--- src/components/PlayerFooter.jsx
+++ src/components/PlayerFooter.jsx
@@ -1,16 +1,10 @@
 import LyricsDisplay from './LyricsDisplay';
 import Waveform from './Waveform';
 
-function PlayerFooter({ currentId, songId, metadata, progress, duration, waveform }) {
+function PlayerFooter({ songId, metadata, progress, duration, waveform }) {
   return (
     <footer className="hidden lg:flex flex-col gap-4 w-full p-4 bg-gray-200 dark:bg-gray-800 rounded-lg">
-      <LyricsDisplay
-        key={currentId}
-        songId={songId}
-        title={metadata.title}
-        artist={metadata.artist}
-        progress={progress}
-      />
+      <LyricsDisplay songId={songId} progress={progress} isPlaying={false} /> {/* Assume not playing in footer */}
       <Waveform waveformData={waveform} progress={progress} duration={duration} />
     </footer>
   );
